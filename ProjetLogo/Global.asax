add11c6214c4f24fdc4dae3c06a64cef4e4133ef
lauvanbe <lauvanbe@gmail.com>
2018-05-24 21:53:43 +0200

    Ajoutez des fichiers projet.

---
+++ ProjetLogo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjetLogo.MvcApplication" Language="C#" %>
---
+++ ProjetLogo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetLogo.Startup))]
+namespace ProjetLogo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
